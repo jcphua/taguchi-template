1bdd5b0093376ed44b2e23e0894aa2ab80d85d52
Ben Dyer <ben.dyer@taguchimail.com>
2012-01-20 01:49:08 +1100

    Fixing link redirection

--- BaseEmail/main.js
+++ BaseEmail/main.js
@@ -120,7 +120,7 @@ BaseEmail.on('click', function(request, response) {
     var link = analytics.parseClickTrackingURL(request.get('/path')), dest;
 
     // if the link contains a format string, parse it and apply the format
-    if (link.indexOf('{%') > -1) {
+    if (link.destination.indexOf('{%') > -1) {
         dest = this.renderString(
                 view.compile(link.destination, false, '{%', '%}'), response);
     } else {
