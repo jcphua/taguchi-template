1e3c82d24afd0dd4bfaff7308b314570e8f038c1
Ben Dyer <ben.dyer@taguchimail.com>
2013-03-21 15:19:41 +1100

    Added license information; removed storage module dependency

--- BaseSMS/main.js
+++ BaseSMS/main.js
@@ -1,8 +1,29 @@
 /*global require: false, module: false, id: false, uri: false */
 
+/*!
+Copyright (C) 2011-2013 TaguchiMarketing Pty Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 var template = require('template'),
     view = require('../view'),
-    storage = require('mimeformat/storage'),
     analytics = require('mimeformat/analytics'),
     util = require('util'),
     mime = require('mime'),
@@ -20,31 +41,13 @@ var template = require('template'),
 module.exports = BaseSMS;
 
 BaseSMS.load(function() {
-    if (!storage.getItem('initialized')) {
-        // Set up stats counters
-        util.each(events, function(ref,name) {
-            storage.stats.zeroCounter(name);
-        });
-        storage.stats.zeroUniqueCounter('BaseSMS.click', 1/256, 104857600);
-        storage.stats.zeroUniqueCounter('BaseSMS.unsubscribe', 1/256,
-            104857600);
-        storage.setItem('initialized');
-    }
-
     this.BaseSMS = {
         baseURL: 'http://' + this.config.hostname + '/'
     };
 });
 
 BaseSMS.request(function(request, response) {
-    // Update base email stats
-    if (!request.test && events[request.ref]) {
-        storage.stats.incrementCounter(events[request.ref]);
-        if (request.ref == 'c' || request.ref == 'u') {
-            storage.stats.updateUniqueCounter(events[request.ref],
-                request.parent.id);
-        }
-    }
+
 });
 
 BaseSMS.on('send.sms', function(request, response) {
--- view.js
+++ view.js
@@ -8,7 +8,7 @@
     under module_name for later calling via apply(). Returns the
     module function.
 
- This JS library is a modified version of jQote2,
+ This JS library is a heavily modified version of jQote2,
  which can be used with no dependencies on jQuery.
  Unit test for this library is tm.template.html.
 */
@@ -16,6 +16,29 @@
 /*global require: false, exports: false, id: false, uri: false,
 global: false */
 /*jshint evil: true */
+
+/*!
+Copyright (C) 2010 aefxx, http://aefxx.com/
+Copyright (C) 2011-2013 TaguchiMarketing Pty Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 var type_of = Object.prototype.toString,
     _e = require('util').xmlFromString;
