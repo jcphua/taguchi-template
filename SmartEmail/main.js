89c833bec9d5ac1441175d8b1b5b396aa89cb343
Ben Dyer <ben.dyer@taguchimail.com>
2013-06-19 13:47:02 +1000

    Synced with latest mimeformat.it SmartEmail version

--- SmartEmail/main.js
+++ SmartEmail/main.js
@@ -1,17 +1,23 @@
 /*global require: false, module: false, id: false, uri: false */
 
 var template = require('template'),
+    util = require('util'),
+    // Create a new template based on an existing template type.
     SmartEmail = template.define('SmartEmail')
-                         .basedOn('BaseEmail');
+                .basedOn('BaseEmail');
 
+// Make the template available to the running application.
 module.exports = SmartEmail;
 
+// Register an on-load handler for this template, which can be used to perform
+// template-specific setup like retrieval of content via HTTP, or complex
+// calculations.
+// The on-load handler is called each time the application starts up.
 SmartEmail.load(function() {
-    var params = {
-        trackingServer: this.BaseEmail.baseURL,
+    this.SmartEmail = {
+        trackingServer: null,
         listUnsubscribeURL: 'http://example.org/unsubscribe',
-        viewOnlineURL: this.BaseEmail.baseURL + '/public/broadcast?sevt=' +
-            '{{ request.id }}&amp;e={{ recipient.hash %}}',
+        viewOnlineURL: null,
         width: 600,
         gutterWidth: 0,
         paddingWidth: 20,
@@ -24,22 +30,43 @@ SmartEmail.load(function() {
         }
     };
 
-    params.contentWidth = params.width - params.gutterWidth*2;
+    this.SmartEmail.contentWidth = this.SmartEmail.width -
+        this.SmartEmail.gutterWidth*2;
 
-    params.itemExteriorWidths = {
-        '1/1': 0 | params.contentWidth/1,
-        '1/2': 0 | params.contentWidth/2,
-        '1/3': 0 | params.contentWidth/3,
-        '1/4': 0 | params.contentWidth/4
+    this.SmartEmail.itemExteriorWidths = {
+        '1/1': 0 | this.SmartEmail.contentWidth/1,
+        '1/2': 0 | this.SmartEmail.contentWidth/2,
+        '1/3': 0 | this.SmartEmail.contentWidth/3,
+        '1/4': 0 | this.SmartEmail.contentWidth/4
     };
 
-    params.paddedItemExteriorWidths = {
-        '1/1': 0 | (params.contentWidth - params.paddingWidth*2)/1,
-        '1/2': 0 | (params.contentWidth - params.paddingWidth*2)/2,
-        '1/3': 0 | (params.contentWidth - params.paddingWidth*2)/3,
-        '1/4': 0 | (params.contentWidth - params.paddingWidth*2)/4
+    this.SmartEmail.paddedItemExteriorWidths = {
+        '1/1': 0 | (this.SmartEmail.contentWidth - this.SmartEmail.paddingWidth*2)/1,
+        '1/2': 0 | (this.SmartEmail.contentWidth - this.SmartEmail.paddingWidth*2)/2,
+        '1/3': 0 | (this.SmartEmail.contentWidth - this.SmartEmail.paddingWidth*2)/3,
+        '1/4': 0 | (this.SmartEmail.contentWidth - this.SmartEmail.paddingWidth*2)/4
     };
 
-    this.SmartEmail = params;
-    this.BaseEmail.inlineCss - true;
+    this.SmartEmail.resizeURL = function(img, w, h) {
+        var uri = "http://ltaguchi.rendr.it/resize/" +
+                util.base64FromBytes(util.bytesFromString(img))
+                .replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '') + ".jpg?";
+
+        if (w) {
+            uri += "w=" + w;
+        }
+        if (h) {
+            url += "h=" + h;
+        }
+
+        return uri;
+    };
+});
+
+SmartEmail.request(function(request, response) {
+    this.SmartEmail.trackingServer = this.BaseEmail.baseURL;
+    this.SmartEmail.viewOnlineURL = this.BaseEmail.baseURL +
+        '/public/broadcast?sevt={% request.id %}&amp;e={% recipient.hash %}';
+    this.BaseEmail.fromAddress = "Taguchi Travel <travel@taguchimail.com>";
+    this.BaseEmail.inlineCss = true;
 });
