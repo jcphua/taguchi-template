beb271e57335f0ee41dc3c57fb8fd785e0d95366
Ben Dyer <ben.dyer@taguchimail.com>
2014-04-29 18:18:12 +1000

    More performance improvements

--- cssinliner.js
+++ cssinliner.js
@@ -7,116 +7,51 @@ var VOID_ELEMENTS = ["area", "base", "br", "col", "command", "embed", "hr",
     "wbr"];
 
 /**
-Consumes consecutive spaces in the input string starting from the index
-(inclusive) until finding a non-space character, and returns the index
-of that first non-space character. If not found, the index exhausts the
-input string.
-*/
-function consume_spaces(index, input) {
-    while (SPACES.test(input[index])) {
-        index++;
-    }
-    return index;
-}
-
-/**
-Consumes consecutive letters in the input string starting from the index
-(inclusive) until finding a non-letter character, and returns the string
-found. If not found, the index exhausts the input string and returns an
-empty string. The caller can calculate the next index from the length of
-the returned string.
-*/
-function consume_letters(index, input) {
-    var startIdx = index;
-    while (('a' <= input[index] && input[index] <= 'z') ||
-            ('A' <= input[index] && input[index] <= 'Z')) {
-        index++;
-    }
-    return input.substring(startIdx, index);
-}
-
-/**
-Consumes consecutive characters within a and b (e.g. quotes) in the input
-string starting from the index (inclusive), and returns the string found.
-If not found, the index exhausts the input string, and the returned value
-is not defined. The first indexed character is expected to be character a.
-The caller can calculate the next index from the length of the returned
-string, which does not include characters a and b.
-*/
-function consume_chars(index, input, a, b) {
-    var startIdx;
-    index++;
-    startIdx = index;
-    while (input[index] !== undefined && input[index] != b) {
-        index++;
-    }
-    return input.substring(startIdx, index);
-}
-
-/**
 Returns {name: string, attributes: {string: string, ...}, end: boolean} from
 the input. The input is the text b/w "<" and ">".
 */
+ATTR_REGEX = /(id|style|class)\s*=\s*('[^']*'|"[^"]*"|[^\s]+)/ig
 function process_start_tag(input) {
     var obj = {attributes: {}, end: false};
     // get the name starting from index 0 until finding a space or "/"
-    var index = 0;
-    obj.name = consume_letters(index, input);
-    index += obj.name.length;
-    // loop to grab attributes (key, value), each attribute starts with at
-    // least a space, and we support the following patterns of attributes:
-    // key | key= | key=val | key='val' | key="val" | key = val (or similar)
-    while (true) {
-        if (SPACES.test(input[index])) {
-            index = consume_spaces(index, input);
-            if (input[index] && LETTERS.test(input[index])) {
-                var key = consume_letters(index, input);
-                index += key.length;
-                index = consume_spaces(index, input);
-                if (input[index] == "=") {
-                    index++;
-                    index = consume_spaces(index, input);
-                    if (input[index] && LETTERS.test(input[index])) {
-                        var val = consume_letters(index, input);
-                        index += val.length;
-                        obj.attributes[key] = val;
-                    } else if (input[index] == "'") {
-                        var val = consume_chars(index, input, "'", "'");
-                        index += val.length + 2;
-                        obj.attributes[key] = val;
-                    } else if (input[index] == '"') {
-                        var val = consume_chars(index, input, '"', '"');
-                        index += val.length + 2;
-                        obj.attributes[key] = val;
-                    } else if (input[index] == "/") {
-                        obj.attributes[key] = "";
-                        break;
-                    } else {
-                        break;
-                    }
-                } else if (input[index] && LETTERS.test(input[index])) {
-                    obj.attributes[key] = null;
-                    index--;  // move back to a space character
-                } else if (input[index] == "/") {
-                    obj.attributes[key] = null;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        } else {
-            break;
+    var match, prevIndex = 0, tokens;
+    tokens = /([\w]+)/.exec(input);
+    obj.name = tokens[0];
+    input = input.substr(obj.name.length);
+
+    obj.miscAttributes = "";
+    while ((match = ATTR_REGEX.exec(input)) !== null) {
+        attrName = match[1];
+        attrValue = match[2];
+        if (attrValue.length &&
+                (attrValue[0] == '"' || attrValue[0] == "'")) {
+            attrValue = attrValue.substr(1, attrValue.length - 2);
         }
+
+        obj.attributes[attrName] = attrValue;
+
+        obj.miscAttributes += input.substring(prevIndex, match.index);
+        prevIndex = match.index + match[0].length;
     }
+    ATTR_REGEX.lastIndex = 0;
+
     // no more attributes to consider, just the end of the start tag
-    if (input[index] == "/") {
+    if (input[input.length - 1] == "/") {
         // this tag is also an end tag
         obj.end = true;
+        obj.miscAttributes += input.substring(prevIndex, input.length - 1);
+    } else {
+        obj.miscAttributes += input.substr(prevIndex);
     }
+
     // for void elements, they are considered start-end tags
     if (VOID_ELEMENTS.indexOf(obj.name) != -1) {
         obj.end = true;
+    }
+    if (obj.attributes.class && obj.attributes.class != "") {
+        obj.classNames = obj.attributes.class.split(/\s+/);
+    } else {
+        obj.classNames = null;
     }
     return obj;
 }
@@ -133,6 +68,7 @@ function to_start_tag(obj) {
             tag += " " + key + "=\"" + obj.attributes[key] + "\"";
         }
     }
+    tag += obj.miscAttributes;
     return "<" + tag + (obj.end ? "/>" : ">");
 }
 
@@ -233,20 +169,23 @@ Returns true if the parents matches the hierarchical rule and false otherwise.
 The order of tags/names in the parents and the rule must be descending.
 */
 function match_hierarchical_rule(parents, rule) {
-    var next = 0;
-    for (var i = 0; i < rule.length; i++) {
-        var r = rule[i], rClassExp = new RegExp("(^|\\s)" + r.class + "($|\\s)");
-        var found = -1;
-        for (var j = next; j < parents.length; j++) {
-            var p = parents[j];
+    var next = 0, r, found, i, il, j, jl, p;
+    jl = parents.length;
+    for (i = 0, il = rule.length; i < il; i++) {
+        r = rule[i];
+        found = -1;
 
+        for (j = next; j < jl; j++) {
+            p = parents[j];
             if ((r.name == "*" || p.name == r.name) &&
                 (r.id == "*" || p.attributes.id == r.id) &&
-                (r.class == "*" || rClassExp.test(p.attributes.class))) {
+                (r.class == "*" ||
+                    (p.classNames && p.classNames.indexOf(r.class) != -1))) {
                 found = j;
                 break;
             }
         }
+
         // return false if found no match; otherwise set the next index
         if (found == -1) {
             return false;
@@ -264,18 +203,20 @@ change is made to the object directly.
 function apply_internal_style(obj, rules, parents) {
     if (rules.length) {
         // parse the inline style of the object to an object
-        var style = obj.attributes.style || "",
-            clsArr = (obj.attributes.class || "").split(/\s+/);
+        var style = "";
         // check with each internal style rule and apply as inline style if matched
-        for (var i = rules.length - 1; i >= 0; i--) {
+        for (var i = 0, l = rules.length; i < l; i++) {
             var rule = rules[i];
             if ((rule.name == "*" || obj.name == rule.name) &&
                 (rule.id == "*" || obj.attributes.id == rule.id) &&
-                (rule.class == "*" || clsArr.indexOf(rule.class) != -1) &&
+                (rule.class == "*" ||
+                    (obj.classNames &&
+                    obj.classNames.indexOf(rule.class) != -1)) &&
                 match_hierarchical_rule(parents, rule.parents)) {
-                style = rule.style + ";" + style;
+                style += rule.style + ";";
             }
         }
+        style += (obj.attributes.style || "");
         // set the value of attribute style if required
         if (style !== "") {
             // replace multiple ;s with just one ;
@@ -292,50 +233,47 @@ module.exports = function(input) {
     var parents = [];
     var rules = [];
     // need to remove spaces before the first tag (a corner case)
-    var rows = input.trim().split("<");
+    var rows = input.trim().split("<"), out = "";
     for (var i = 0; i < rows.length; i++) {
         var row = rows[i];
         if (row !== "") {
             // HTML comment <!-- ... -->, skip
-            if (rows[i].substr(0, 3) == "!--") {
+            if (row.substr(0, 3) == "!--") {
                 for ( ; i < rows.length; i++) {
-                    rows[i] = ["<", rows[i]].join("");
-                    if (rows[i].trim().substr(rows[i].trim().length - 3, 3) == "-->") {
+                    out += "<" + rows[i];
+                    if (/-->\s*$/.test(rows[i])) {
                         break;
                     }
                 }
                 continue;
             }
-            // HTML content, process
-            var tokens = null;
-            if (row.substring(0, 5) == "style") {
-                tokens = ["style", row.substring(6)];
-            } else {
-                tokens = row.split(">", 2);
-            }
-            var tag = tokens[0];
-            if (tag[0] && LETTERS.test(tag[0])) { // start tag (perhaps start-end tag)
-                var obj = process_start_tag(tag);
-                apply_internal_style(obj, rules, parents);
-                rows[i] = [to_start_tag(obj), tokens[1]].join("");
-                if (!obj.end) {
-                    parents.unshift(obj);
+            if (row[0] && LETTERS.test(row[0])) { // start tag (perhaps start-end tag)
+                if (row.substring(0, 5) == "style") {
+                    // specifically consume content of <style> to build rules
+                    rules = parse_internal_style(row.substring(6));
+                    out += "<" + row;
+                } else {
+                    // HTML content, process
+                    var tokens, obj;
+                    tokens = row.split(">", 2);
+                    obj = process_start_tag(tokens[0]);
+                    apply_internal_style(obj, rules, parents);
+                    out += to_start_tag(obj) + tokens[1];
+                    if (!obj.end) {
+                        parents.unshift(obj);
+                    }
                 }
-                // specifically consume content of <style> to build rules
-                if (obj.name == "style") {
-                    rules = parse_internal_style(tokens[1]);
-                }
-            } else if (tag[0] == "/") { // end tag
-                var name = consume_letters(1, tag);
+            } else if (row[0] == "/") { // end tag
                 // pop the most recent parent out if the name matches
-                if (parents.length && parents[0].name == name) {
+                if (parents.length &&
+                        new RegExp("^/" + parents[0].name).test(row)) {
                     parents.shift();
                 }
-                rows[i] = ["</", name, ">", tokens[1]].join("");
+                out += "<" + row;
             } else { // unknown stuff; just emit as is
-                rows[i] = ["<", row].join("");
+                out += "<" + row;
             }
         }
     }
-    return rows.join("");
+    return out;
 }
